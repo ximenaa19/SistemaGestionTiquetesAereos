afd0637083e87b62cf4c8c291937509223d39eef
karen ximena afanador suarez <222234207+ximenaa19@users.noreply.github.com>
2026-04-18 12:40:13 -0500

    feat: :sparkles: NewMigrations

---
+++ Migrations/20260418173919_FlightStatusMigration.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GestionAerolineas.Migrations
+{
+    /// <inheritdoc />
+    public partial class FlightStatusMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "estados_vuelo",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    nombre = table.Column<string>(type: "varchar(50)", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_estados_vuelo", x => x.id);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_estados_vuelo_nombre",
+                table: "estados_vuelo",
+                column: "nombre",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "estados_vuelo");
+        }
+    }
+}
