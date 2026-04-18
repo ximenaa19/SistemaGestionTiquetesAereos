78f3d169ce4d70b04c5e9e7935a36a361cef7c72
karen ximena afanador suarez <222234207+ximenaa19@users.noreply.github.com>
2026-04-18 17:40:00 -0500

    feat: :sparkles: staffRoles migration

---
+++ Migrations/20260418223935_StaffRolesMigration.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GestionAerolineas.Migrations
+{
+    /// <inheritdoc />
+    public partial class StaffRolesMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "cargos_personal",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    nombre = table.Column<string>(type: "varchar(100)", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_cargos_personal", x => x.id);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_cargos_personal_nombre",
+                table: "cargos_personal",
+                column: "nombre",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "cargos_personal");
+        }
+    }
+}
