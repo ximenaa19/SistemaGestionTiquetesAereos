066fa4d00f97a7f3b38e236c9bd1c3836efc9450
karen ximena afanador suarez <222234207+ximenaa19@users.noreply.github.com>
2026-04-18 17:58:09 -0500

    feat: :sparkles: Seasons migration

---
+++ Migrations/20260418225730_SeasonsMigration.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GestionAerolineas.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeasonsMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "temporadas",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    nombre = table.Column<string>(type: "varchar(50)", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    descripcion = table.Column<string>(type: "varchar(150)", nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    precio_factor = table.Column<decimal>(type: "decimal(5,4)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_temporadas", x => x.id);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_temporadas_nombre",
+                table: "temporadas",
+                column: "nombre",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "temporadas");
+        }
+    }
+}
