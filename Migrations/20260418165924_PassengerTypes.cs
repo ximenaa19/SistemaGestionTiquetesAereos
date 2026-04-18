31b07f1755cf3507b984206eb9ae7d24fe14dc45
MLahitton <222233533+MLahitton@users.noreply.github.com>
2026-04-18 12:00:53 -0500

    Migraciones

---
+++ Migrations/20260418165924_PassengerTypes.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GestionAerolineas.Migrations
+{
+    /// <inheritdoc />
+    public partial class PassengerTypes : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "tipos_pasajero",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    nombre = table.Column<string>(type: "varchar(50)", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    edad_min = table.Column<int>(type: "int", nullable: true),
+                    edad_max = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_tipos_pasajero", x => x.id);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_tipos_pasajero_nombre",
+                table: "tipos_pasajero",
+                column: "nombre",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "tipos_pasajero");
+        }
+    }
+}
