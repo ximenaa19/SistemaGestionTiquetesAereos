dc888d1565e7d334027a701f64613662725a360e
karen ximena afanador suarez <222234207+ximenaa19@users.noreply.github.com>
2026-04-20 15:56:20 -0500

    feat: :sparkles: Airports migration

---
+++ Migrations/20260420205545_AirportsMigration.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GestionAerolineas.Migrations
+{
+    /// <inheritdoc />
+    public partial class AirportsMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "airports",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    nombre = table.Column<string>(type: "varchar(150)", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    codigo_iata = table.Column<string>(type: "varchar(3)", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    codigo_icao = table.Column<string>(type: "varchar(4)", nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    ciudad_id = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_airports", x => x.id);
+                    table.ForeignKey(
+                        name: "FK_airports_cities_ciudad_id",
+                        column: x => x.ciudad_id,
+                        principalTable: "cities",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Restrict);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_airports_ciudad_id",
+                table: "airports",
+                column: "ciudad_id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_airports_codigo_iata",
+                table: "airports",
+                column: "codigo_iata",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_airports_codigo_icao",
+                table: "airports",
+                column: "codigo_icao",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "airports");
+        }
+    }
+}
