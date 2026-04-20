3edf58da38ac50edf1274c9d283d304b017017eb
MLahitton <222233533+MLahitton@users.noreply.github.com>
2026-04-20 14:31:26 -0500

    feat: :sparkles: rename 'name' column to 'nombre' in cities table

---
+++ Migrations/20260420192852_RenameCitiesNameToNombre.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GestionAerolineas.Migrations
+{
+    /// <inheritdoc />
+    public partial class RenameCitiesNameToNombre : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "name",
+                table: "cities",
+                newName: "nombre");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "nombre",
+                table: "cities",
+                newName: "name");
+        }
+    }
+}
