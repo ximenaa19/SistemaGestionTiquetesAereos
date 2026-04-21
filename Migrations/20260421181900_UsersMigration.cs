b4283bedbed6134e717cc3a7a106d20ea07e55f8
karen ximena afanador suarez <222234207+ximenaa19@users.noreply.github.com>
2026-04-21 13:21:18 -0500

    feat: :sparkles: User Migration

---
+++ Migrations/20260421181900_UsersMigration.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GestionAerolineas.Migrations
+{
+    /// <inheritdoc />
+    public partial class UsersMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "payments",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    reserva_id = table.Column<int>(type: "int", nullable: false),
+                    monto = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    fecha_pago = table.Column<DateTime>(type: "datetime", nullable: false),
+                    estado_pago_id = table.Column<int>(type: "int", nullable: false),
+                    metodo_pago_id = table.Column<int>(type: "int", nullable: false),
+                    creado_en = table.Column<DateTime>(type: "datetime", nullable: true, defaultValueSql: "CURRENT_TIMESTAMP"),
+                    actualizado_en = table.Column<DateTime>(type: "datetime", nullable: true, defaultValueSql: "CURRENT_TIMESTAMP")
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.ComputedColumn)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_payments", x => x.id);
+                    table.ForeignKey(
+                        name: "FK_payments_estados_pago_estado_pago_id",
+                        column: x => x.estado_pago_id,
+                        principalTable: "estados_pago",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_payments_paymentmethods_metodo_pago_id",
+                        column: x => x.metodo_pago_id,
+                        principalTable: "paymentmethods",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_payments_reservations_reserva_id",
+                        column: x => x.reserva_id,
+                        principalTable: "reservations",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Restrict);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_payments_estado_pago_id",
+                table: "payments",
+                column: "estado_pago_id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_payments_metodo_pago_id",
+                table: "payments",
+                column: "metodo_pago_id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_payments_reserva_id",
+                table: "payments",
+                column: "reserva_id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "payments");
+        }
+    }
+}
