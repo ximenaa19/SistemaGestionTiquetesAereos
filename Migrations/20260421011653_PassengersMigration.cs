0658d43a2572122b6ce611bac9cf4720ce7aa3a7
karen ximena afanador suarez <222234207+ximenaa19@users.noreply.github.com>
2026-04-20 20:17:21 -0500

    feat: :sparkles: Passengers Migration

---
+++ Migrations/20260421011653_PassengersMigration.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GestionAerolineas.Migrations
+{
+    /// <inheritdoc />
+    public partial class PassengersMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "passengers",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    persona_id = table.Column<int>(type: "int", nullable: false),
+                    tipo_pasajero_id = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_passengers", x => x.id);
+                    table.ForeignKey(
+                        name: "FK_passengers_people_persona_id",
+                        column: x => x.persona_id,
+                        principalTable: "people",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_passengers_tipos_pasajero_tipo_pasajero_id",
+                        column: x => x.tipo_pasajero_id,
+                        principalTable: "tipos_pasajero",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Restrict);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_passengers_persona_id",
+                table: "passengers",
+                column: "persona_id",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_passengers_tipo_pasajero_id",
+                table: "passengers",
+                column: "tipo_pasajero_id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "passengers");
+        }
+    }
+}
